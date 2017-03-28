f66498488fd0ff642dc1fb4e6b5ec1e9ec998677
takaaki <takaaki@razil.jp>
2017-03-28 19:26:13 +0900

    miyoutv: angular-md5モジュールを追加

--- miyoutv/src/app/main.js
+++ miyoutv/src/app/main.js
@@ -157,7 +157,7 @@ global.module.paths.push(modulePath);
       $window.resizeTo(windowState.width, windowState.height);
     }
   }
-  angular.module('app', ['ngAnimate', 'ngRoute', 'ui.bootstrap', 'agGrid', 'cfp.hotkeys', 'toaster', 'angular-loading-bar'])
+  angular.module('app', ['ngAnimate', 'ngRoute', 'ui.bootstrap', 'agGrid', 'cfp.hotkeys', 'toaster', 'angular-loading-bar', 'angular-md5'])
     .constant('commentUrl', commentUrl)
     .constant('commentTokenUrl', commentTokenUrl)
     .constant('categoryTable', categoryTable)
