a78509ffd91f8f3634255cd7e57a1b2240cec29e
takaaki <takaaki@razil.jp>
2017-06-28 15:42:04 +0900

    webpackの設定を統合

---
+++ webpack.config.js
@@ -0,0 +1,109 @@
+const path = require('path');
+const webpack = require('webpack');
+const glob = require('glob');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const miyoutvConfigJs = {
+  entry: {
+    app: glob.sync(path.join(__dirname, 'miyoutv/src/app/**/*.js'), {
+      nosort: true,
+    }),
+    index: path.join(__dirname, 'miyoutv/src/index.js'),
+  },
+  output: {
+    path: path.join(__dirname, 'miyoutv/dist/'),
+    filename: '[name].js',
+  },
+  module: {
+    noParse: /\.js$/,
+    rules: [{
+      test: /\.js$/,
+      use: 'ng-annotate-loader',
+    }],
+  },
+  devtool: 'source-map',
+  plugins: [
+    new webpack.DefinePlugin(
+      process.env.IS_PACK ? {
+        NODE_ENV: process.env.NODE_ENV,
+        GARAPON_DEVID: process.env.GARAPON_DEVID,
+      } : {}
+    ),
+    new UglifyJSPlugin({
+      sourceMap: process.env.NODE_ENV !== 'production',
+    }),
+  ]
+};
+const miyoutvConfigBundle = {
+  entry: {
+    bundle: [
+      path.join(__dirname, 'miyoutv/src/bundle.js'),
+      path.join(__dirname, 'miyoutv/src/bundle.css'),
+    ],
+  },
+  output: {
+    path: path.join(__dirname, 'miyoutv/dist/'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: ExtractTextPlugin.extract({
+        use: `css-loader?${JSON.stringify({
+          minimize: true,
+          sourceMap: process.env.NODE_ENV !== 'production',
+        })}`,
+      }),
+    }, {
+      test: /(typeface|fonts).+\.(eot|otf|svg|ttf|woff2?)$/,
+      use: 'file-loader?name=fonts/[name].[ext]',
+    }]
+  },
+  devtool: 'source-map',
+  plugins: [
+    new UglifyJSPlugin({
+      sourceMap: process.env.NODE_ENV !== 'production',
+    }),
+    new ExtractTextPlugin('[name].css'),
+  ]
+};
+const agentConfig = {
+  entry: {
+    'miyoutv-agent': path.join(__dirname, 'miyoutv-agent/src/miyoutv-agent.js'),
+    'tools/servicelist': path.join(__dirname, 'miyoutv-agent/src/servicelist.js'),
+  },
+  output: {
+    path: path.join(__dirname, 'build/miyoutv-agent/'),
+    filename: '[name].js',
+  },
+  target: 'node',
+  node: {
+    console: false,
+    global: false,
+    process: false,
+    Buffer: false,
+    __filename: false,
+    __dirname: false,
+  },
+};
+
+module.exports = (env) => {
+  const config = [];
+  if (env.miyoutv || !env.agent) {
+    if (env.js) {
+      config.push(miyoutvConfigJs);
+    }
+    if (env.bundle) {
+      config.push(miyoutvConfigBundle);
+    }
+    if (config.length < 1) {
+      config.push(miyoutvConfigJs);
+      config.push(miyoutvConfigBundle);
+    }
+  }
+  if (env.agent) {
+    config.push(agentConfig);
+  }
+  return config;
+};
