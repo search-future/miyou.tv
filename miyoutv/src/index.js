ea280b03ac2dc6643f3591588bd2c757e619a22c
takaaki <takaaki@razil.jp>
2017-07-03 20:00:03 +0900

    miyoutv: アプリケーションメニューを作成

--- miyoutv/src/index.js
+++ miyoutv/src/index.js
@@ -14,7 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 var app = require('electron').app;
+var shell = require('electron').shell;
 var BrowserWindow = require('electron').BrowserWindow;
+var Menu = require('electron').Menu;
 
 var path = require('path');
 var fs = require('fs');
@@ -39,17 +41,130 @@ if (process.platform === 'win32' && !process.env.VLC_PLUGIN_PATH) {
   }
 }
 
+function buildAppMenu() {
+  var template = [];
+
+  if (process.platform === 'darwin') {
+    template.unshift({
+      label: app.getName(),
+      submenu: [{
+        label: app.getName() + 'について',
+        role: 'about'
+      }, {
+        type: 'separator'
+      }, {
+        label: app.getName() + 'を隠す',
+        role: 'hide'
+      }, {
+        label: 'ほかを隠す',
+        role: 'hideothers'
+      }, {
+        label: 'すべてを表示',
+        role: 'unhide'
+      }, {
+        type: 'separator'
+      }, {
+        label: 'MiyouTVを終了',
+        role: 'quit',
+        click: function () {
+          app.quit();
+        }
+      }]
+    });
+  } else {
+    template.push({
+      label: 'ファイル',
+      submenu: [{
+        label: 'MiyouTVを終了',
+        role: 'quit',
+        click: function () {
+          app.quit();
+        }
+      }]
+    });
+  }
+  template.push({
+    label: '編集',
+    submenu: [{
+      label: '元に戻す',
+      role: 'undo'
+    }, {
+      label: 'やり直す',
+      role: 'redo'
+    }, {
+      type: 'separator'
+    }, {
+      label: '切り取り',
+      role: 'cut'
+    }, {
+      label: 'コピー',
+      role: 'copy'
+    }, {
+      label: '貼り付け',
+      role: 'paste'
+    }, {
+      label: '削除',
+      role: 'delete',
+      click: function () {
+        win.webContents.delete();
+      }
+    }, {
+      type: 'separator'
+    }, {
+      label: 'すべて選択',
+      role: 'selectall'
+    }]
+  });
+  template.push({
+    label: '表示',
+    submenu: [{
+      label: 'MiyouTVを再読み込み',
+      role: 'reload',
+      key: 'CmdOrCtrl+R',
+      click: function () {
+        win.webContents.reload();
+      }
+    }, {
+      label: '開発者ツール',
+      role: 'toggledevtools',
+      key: 'CmdOrCtrl+Shift+I',
+      click: function () {
+        win.webContents.toggleDevTools();
+      }
+    }, {
+      type: 'separator'
+    }, {
+      label: '全画面表示',
+      role: 'togglefullscreen',
+      key: 'F11',
+      click: function () {
+        win.setFullScreen(!win.isFullScreen());
+      }
+    }]
+  });
+  template.push({
+    label: 'ヘルプ',
+    submenu: [{
+      label: app.getName() + 'について',
+      click: function () {
+        shell.openExternal('http://miyou.tv');
+      }
+    }]
+  });
+
+  return Menu.buildFromTemplate(template);
+}
+
 function createWindow() {
   win = new BrowserWindow({
     minWidth: 300,
     minHeight: 500,
+    autoHideMenuBar: true,
     show: nodeEnv !== 'production'
   });
 
   if (nodeEnv !== 'production') {
     win.webContents.openDevTools();
-  } else {
-    win.setMenu(null);
   }
 
   win.loadURL('file://' + path.join(__dirname, '/index.html'));
@@ -57,6 +172,8 @@ function createWindow() {
   win.on('closed', function () {
     win = null;
   });
+
+  Menu.setApplicationMenu(buildAppMenu());
 }
 
 app.on('ready', createWindow);
