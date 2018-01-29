7cd471f6b5c6d2b076b64c08f96b1aa7b7853b3c
takaaki <takaaki@razil.jp>
2018-01-29 14:35:47 +0900

    miyoutv: AppComponentにウインドウ状態記憶機能を追加

--- miyoutv/src/app/app.component.ts
+++ miyoutv/src/app/app.component.ts
@@ -13,24 +13,31 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { StorageService } from './shared/storage.service';
+import { WindowService } from './shared/window.service';
 
 @Component({
   selector: 'miyoutv',
   templateUrl: 'app.component.html',
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   constructor(
     private storageService: StorageService,
+    private windowService: WindowService,
   ) { }
 
   public ngOnInit() {
     if (!this.storageService.loadSessionStorage('isLoaded')) {
       this.upgradeSetting();
+      this.loadWindowState();
     }
     this.storageService.saveSessionStorage('isLoaded', true);
+  }
+
+  public ngOnDestroy() {
+    this.saveWindowState();
   }
 
   protected upgradeSetting() {
@@ -45,5 +52,28 @@ export class AppComponent implements OnInit {
     this.storageService.removeLocalStorage('commentCountCache');
     this.storageService.removeLocalStorage('comment_token');
     this.storageService.removeFile('comments', 'commentCache.json');
+  }
+
+  public saveWindowState() {
+    if (!(
+      this.windowService.isMinimized ||
+      this.windowService.isMaximized ||
+      this.windowService.fullscreen
+    )) {
+      this.storageService.saveLocalStorage('windowState', {
+        x: window.screenX,
+        y: window.screenY,
+        width: window.outerWidth,
+        height: window.outerHeight,
+      });
+      this.storageService.saveLocalStorage('alwaysOnTop', this.windowService.alwaysOnTop);
+    }
+  }
+
+  public loadWindowState() {
+    const windowState: any = this.storageService.loadLocalStorage('windowState') || {};
+    window.moveTo(windowState.x, windowState.y);
+    window.resizeTo(windowState.width, windowState.height);
+    this.windowService.alwaysOnTop = this.storageService.loadLocalStorage('alwaysOnTop') === true;
   }
 }
