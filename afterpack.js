82ce81d262792f8ca3bcbcde6cf7d3a394ef6017
takaaki <takaaki@razil.jp>
2018-04-25 13:24:20 +0900

    miyoutv: mpv.js向けにビルドを調整

--- afterpack.js
+++ afterpack.js
@@ -6,4 +6,8 @@ exports.default = (context) => {
     const libffmpeg = path.join(context.appOutDir, 'libffmpeg.so');
     execSync(`gcc -Wl,--no-as-needed -shared -lavformat -o "${libffmpeg}"`);
   }
+  if (context.electronPlatformName === 'win32') {
+    const downloader = path.join(__dirname, 'download.js');
+    execSync(`node ${downloader}`);
+  }
 };
