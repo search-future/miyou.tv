faca08ea6789a8532f36363c265d4cf2a9fdbc95
takaaki <takaaki@razil.jp>
2019-04-12 18:27:05 +0900

    add react-native-video

--- android/app/src/main/java/jp/razil/miyoutv/MainApplication.java
+++ android/app/src/main/java/jp/razil/miyoutv/MainApplication.java
@@ -3,6 +3,7 @@ package jp.razil.miyoutv;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.brentvatne.react.ReactVideoPackage;
 import com.yuanzhou.vlc.ReactVlcPlayerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactVideoPackage(),
             new ReactVlcPlayerPackage(),
             new VectorIconsPackage()
       );
