8f62b1ceabc4a8c9f891afa0db3379e7979b102c
takaaki <takaaki@razil.jp>
2020-02-04 16:34:00 +0900

    load dropped files and argument files

--- src/utils/init/index.web.ts
+++ src/utils/init/index.web.ts
@@ -16,6 +16,7 @@ import { remote, ipcRenderer } from "electron";
 import Toast from "react-native-root-toast";
 import Mousetrap from "mousetrap";
 
+import { FileActions } from "../../modules/file";
 import { ServiceActions } from "../../modules/service";
 import { SettingActions } from "../../modules/setting";
 import { ViewerActions } from "../../modules/viewer";
@@ -26,6 +27,8 @@ import common from "./common";
 export default function init(store: Store) {
   let mode: "stack" | "view" | "child";
   let boundsSettingName = "";
+  document.addEventListener("dragover", e => e.preventDefault());
+  document.body.addEventListener("drop", dropFileDispatcher);
   if (location.hash.indexOf("view") >= 0) {
     mode = "view";
   } else if (location.hash.indexOf("child") >= 0) {
@@ -55,6 +58,11 @@ export default function init(store: Store) {
       store.dispatch(ServiceActions.commentInit());
       return false;
     });
+    const { argv }: { argv: string[] } = remote.process;
+    const paths = argv.slice(1).filter(a => a !== ".");
+    if (paths.length > 0) {
+      store.dispatch(FileActions.add(paths.map(path => `file://${path}`)));
+    }
     common(store);
   }
   store.dispatch(ViewerActions.init(mode));
@@ -126,5 +134,19 @@ function dispatchWindow(action: AnyAction) {
       ...toastOptions,
       duration: Toast.durations.SHORT
     });
+  }
+}
+
+function dropFileDispatcher({ dataTransfer }: DragEvent) {
+  if (dataTransfer) {
+    const { files } = dataTransfer;
+    const uris = [];
+    for (let i = 0; i < files.length; i++) {
+      const file = files.item(i);
+      if (file) {
+        uris.push(`file://${file.path}`);
+      }
+    }
+    dispatchWindow(FileActions.add(uris));
   }
 }
