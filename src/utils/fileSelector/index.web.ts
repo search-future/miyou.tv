37faad36b101e3190e43349fea63c7f10153e7f8
takaaki <takaaki@razil.jp>
2020-02-04 14:58:16 +0900

    fileSelector util

---
+++ src/utils/fileSelector/index.web.ts
@@ -0,0 +1,41 @@
+/*!
+Copyright 2016-2019 Brazil Ltd.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import { remote } from "electron";
+
+export default async function fileSelector({
+  title,
+  buttonLabel,
+  type: filters,
+  multiSelections
+}: {
+  title?: string;
+  buttonLabel?: string;
+  type?: { name: string; extensions: string[] }[];
+  multiSelections?: boolean;
+}) {
+  const properties: "multiSelections"[] = [];
+  if (multiSelections) {
+    properties.push("multiSelections");
+  }
+  const paths = remote.dialog.showOpenDialog({
+    title,
+    buttonLabel,
+    filters,
+    properties
+  });
+
+  if (paths) {
+    return paths.map(path => `file://${path}`);
+  }
+}
