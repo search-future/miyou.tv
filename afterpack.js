afa0bd01b212c6a1a3be9f5aee74144e4c2a2f53
takaaki <takaaki@razil.jp>
2020-09-04 12:27:01 +0900

    improve compatibility on Linux

--- afterpack.js
+++ afterpack.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const fs = require("fs");
+const { execSync } = require("child_process");
 
 exports.default = context => {
   if (context.electronPlatformName === "linux") {
@@ -11,5 +12,7 @@ exports.default = context => {
     const wrapperPath = path.join(__dirname, "electron-wrapper.sh");
     fs.renameSync(execPath, appPath);
     fs.copyFileSync(wrapperPath, execPath);
+    const libffmpeg = path.join(context.appOutDir, "libffmpeg.so");
+    execSync(`gcc -Wl,--no-as-needed -shared -lavformat -Wl,-rpath,/tmp/miyoutv -o "${libffmpeg}"`);
   }
 };
--- electron-wrapper.sh
+++ electron-wrapper.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
+LAVFORMAT=$(ldconfig -p | sed -n -e 's/^.*=>\s*\(.*libavformat\.so\.[0-9]\+\)$/\1/p')
+
+if [ -e "$LAVFORMAT" ]
+then
+  SONAME=$(ldd "$(cd $(dirname $0) && pwd)/libffmpeg.so" | sed -n -e 's/^.*\(libavformat\.so\.[0-9]\+\).*$/\1/p')
+  mkdir -p /tmp/miyoutv/
+  ln -fs "$LAVFORMAT" "/tmp/miyoutv/$SONAME"
+fi
 
 exec "$(cd $(dirname $0) && pwd)/miyoutv-bin" --no-sandbox "$@"
