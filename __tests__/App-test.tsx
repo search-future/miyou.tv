f015a3f9bdf047e223e9a036fd0808d83068d52e
takaaki <takaaki@razil.jp>
2021-03-09 16:49:27 +0900

    update react-native files

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -1,11 +1,10 @@
 /**
  * @format
- * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
 import 'react-native';
 import React from 'react';
-import App from '../App';
+import App from '../src/App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
